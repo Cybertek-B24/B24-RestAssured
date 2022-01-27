b91928679b8defaf7fe00d8208c540077a65902d
Murodil <murodil@cybertekschool.com>
2022-01-26 22:03:03 -0500

    DAY 02 - headers test

--- src/test/java/com/cybertek/tests/day02_headers_params/SpartanGetRequestsApiTest.java
+++ src/test/java/com/cybertek/tests/day02_headers_params/SpartanGetRequestsApiTest.java
@@ -1,6 +1,7 @@
 package com.cybertek.tests.day02_headers_params;
 
 import com.cybertek.utilities.ConfigurationReader;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.*;
@@ -25,7 +26,34 @@ public class SpartanGetRequestsApiTest {
 
         response.prettyPrint();
         assertTrue(response.asString().contains("Correy"));
+    }
 
+    /**
+     * Given Accept type is "application/json"
+     * When I send a GET request to
+     * spartan_base_url/api/spartans
+     * Then Response STATUS CODE must be 200
+     * And content type should be "application/json"
+     */
+
+    @Test
+    public void allSpartansHeadersTest() {
+        Response response = given().accept(ContentType.JSON). //add header to request
+                             when().get(baseUrl + "/api/spartans");
+
+        System.out.println("status code = " + response.statusCode());
+        assertEquals(200, response.statusCode());
+
+        System.out.println(response.contentType()); //read Content Type response header
+
+        assertEquals("application/json", response.contentType());
+
+        //print more response headers
+        System.out.println("date header value = " + response.getHeader("Date"));
+        System.out.println("transfer encoding = " + response.getHeader("Transfer-Encoding"));
+        //verify header "Date" is present
+        assertTrue(response.getHeaders().hasHeaderWithName("Date"));
     }
 
 }
+
