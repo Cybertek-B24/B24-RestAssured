9502b4a844aa741f63e74057a514ee5e9a240c17
Murodil <murodil@cybertekschool.com>
2022-01-27 20:55:53 -0500

    DAY 03 - params

--- src/test/java/com/cybertek/tests/day03_api_parameters/SpartanWithApiParamsTest.java
+++ src/test/java/com/cybertek/tests/day03_api_parameters/SpartanWithApiParamsTest.java
@@ -43,4 +43,31 @@ public class SpartanWithApiParamsTest {
 
     }
 
+    /**
+     * Given Accept type is json
+     * And path parameter id is 1000
+     * When I send request to /api/spartans/1000
+     * -----------------
+     * Then status code should be 404
+     * And content type should be "application/json"
+     * and json body should contain "Not Found"
+     */
+
+    @Test
+    public void getSpartanPathParamNegativeTest() {
+        Response response = given().accept(ContentType.JSON)
+                .and().pathParam("id",1000)
+                .when().get("/api/spartans/{id}");
+
+        System.out.println("status line = " + response.statusLine());
+        assertEquals(404, response.statusCode());
+
+        System.out.println("content type = " + response.contentType());
+        assertEquals("application/json", response.contentType());
+
+        response.prettyPrint();
+        assertTrue(response.asString().contains("Not Found"));
+
+    }
+
 }
