8d9ba9cfbd8bd4d0fb1dd38499d83f43377740d7
Murodil <murodil@cybertekschool.com>
2022-01-27 20:31:27 -0500

    DAY 03 - params

--- src/test/java/com/cybertek/tests/day03_api_parameters/SpartanWithApiParamsTest.java
+++ src/test/java/com/cybertek/tests/day03_api_parameters/SpartanWithApiParamsTest.java
@@ -1,23 +1,27 @@
 package com.cybertek.tests.day03_api_parameters;
 
 import com.cybertek.utilities.ConfigurationReader;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SpartanWithApiParamsTest {
 
-    @BeforeAll
-    public void setUp() {
+    @BeforeAll //RUN ONCE BEFORE ALL TESTS
+    public static void setUp() {
+        System.out.println("Set up method: assigning value to baseURI variable");
         baseURI = ConfigurationReader.getProperty("spartan.url");
     }
 
     /**
-     *
      Given Accept type is json
      And path parameter id is 24
      When I send request to /api/spartans/24
+     ----
      Then status code should be 200
      And content type should be "application/json"
      and json body should contain "Correy"
@@ -25,6 +29,17 @@ public class SpartanWithApiParamsTest {
 
     @Test
     public void getSpartanPathParamTest() {
+        Response response = given().accept(ContentType.JSON)
+                .when().get("/api/spartans/24");
+
+        System.out.println("status code = " + response.statusCode());
+        assertEquals(200, response.statusCode());
+
+        System.out.println("content type = " + response.contentType());
+        assertEquals("application/json", response.contentType());
+
+        response.prettyPrint();
+        assertTrue(response.asString().contains("Correy"));
 
     }
 
