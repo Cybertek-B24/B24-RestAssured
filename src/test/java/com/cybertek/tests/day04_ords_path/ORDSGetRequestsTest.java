b77a49785e4a0dd60409a49b028a97d98aa84466
Murodil <murodil@cybertekschool.com>
2022-01-29 11:47:41 -0500

    DAY 04 - ords

--- src/test/java/com/cybertek/tests/day04_ords_path/ORDSGetRequestsTest.java
+++ src/test/java/com/cybertek/tests/day04_ords_path/ORDSGetRequestsTest.java
@@ -1,20 +1,47 @@
 package com.cybertek.tests.day04_ords_path;
 
+
 import com.cybertek.utilities.ConfigurationReader;
+import io.restassured.http.ContentType;
+
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ORDSGetRequestsTest {
+
     @BeforeAll
     public static void setUp() {
-        baseURI = ConfigurationReader.getProperty("ords.url");
+        baseURI = ConfigurationReader.getProperty("ords.url"); //http://54.205.239.177:1000/ords/hr
     }
 
+    /**
+     * Given accept type is json
+     * When user send get request to /ords/hr/regions
+     * Status code should be 200
+     * Content type should be "application/json"
+     * And body should contain "Europe"
+     */
+
+    @DisplayName("GET Request to /ords/hr/regions")
     @Test
     public void getAllRegionsTest() {
+        Response response = given().accept(ContentType.JSON)
+                .when().get("/regions");
+
+        System.out.println("status code = " + response.statusCode());
+        assertEquals(200 , response.statusCode());
+
+        System.out.println("Content type = " + response.contentType());
+        assertEquals("application/json", response.contentType());
+
+        response.prettyPrint();
+
+        assertTrue(response.asString().contains("Europe"));
 
     }
 
