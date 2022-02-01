5d9f709fc0f86093891ff9324743aa6d3056c13f
Murodil <murodil@cybertekschool.com>
2022-02-01 17:14:12 -0500

    office hours -  petstore get inventory

--- src/test/java/com/cybertek/tests/office_hours/PetStoreGetRequestsTest.java
+++ src/test/java/com/cybertek/tests/office_hours/PetStoreGetRequestsTest.java
@@ -1,9 +1,12 @@
 package com.cybertek.tests.office_hours;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeAll;
 
 import static io.restassured.RestAssured.*;
-import org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class PetStoreGetRequestsTest {
@@ -15,14 +18,27 @@ public class PetStoreGetRequestsTest {
 
     /**
      * accept type is json
-     * get request to /store/inventory
+     * get request to https://petstore.swagger.io/v2/store/inventory
      * Then status code is 200
      * And content type is json
-     * and available is 573
+     * And date header should be present
+     * and available is more than 500
      */
 
     @Test
     public void getInventoryTest() {
+        Response response = given().accept(ContentType.JSON)
+                .when().get("/store/inventory");
+
+        System.out.println("Status code = " + response.statusCode());
+        assertEquals(HttpStatus.SC_OK, response.statusCode());
+
+        System.out.println("content type = " + response.contentType());
+        System.out.println("date = " + response.getHeader("Date"));
+
+        assertEquals("application/json", response.contentType());
+        assertTrue(response.getHeaders().hasHeaderWithName("Date"));
+
 
     }
 
