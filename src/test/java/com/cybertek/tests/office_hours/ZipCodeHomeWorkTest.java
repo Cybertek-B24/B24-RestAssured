14e36ea483743dfe6b8389c0b9307b073fb828ef
Murodil <murodil@cybertekschool.com>
2022-02-03 17:05:10 -0500

    Office hours - zipcode task

---
+++ src/test/java/com/cybertek/tests/office_hours/ZipCodeHomeWorkTest.java
@@ -0,0 +1,63 @@
+package com.cybertek.tests.office_hours;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ZipCodeHomeWorkTest {
+
+    @BeforeAll
+    public static void setUp() {
+        baseURI = "http://api.zippopotam.us";
+    }
+
+    /**
+     Given Accept application/json
+     And path zipcode is 22031
+     When I send a GET request to /us endpoint
+     Then status code must be 200
+     And content type must be application/json
+     And Server header is cloudflare
+     And Report-To header exists
+     And body should contains following information
+     post code is 22031
+     country  is United States
+     country abbreviation is US
+     place name is Fairfax
+     state is Virginia
+     latitude is 38.8604
+     */
+
+    @Test
+    public void zipcodeTest() {
+        Response response = given().accept(ContentType.JSON)
+                .and().pathParam("postal-code" , 22031)
+                .when().get("/us/{postal-code}");
+
+        System.out.println("status code = " + response.statusCode());
+        assertEquals(200 , response.statusCode());
+
+        assertEquals("application/json" , response.contentType());
+
+        System.out.println("server = " + response.getHeader("Server"));
+        assertEquals("cloudflare" , response.getHeader("Server"));
+
+        System.out.println("report to exists? = " + response.getHeaders().hasHeaderWithName("Report-To"));
+        assertTrue(response.getHeaders().hasHeaderWithName("Report-To"));
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
