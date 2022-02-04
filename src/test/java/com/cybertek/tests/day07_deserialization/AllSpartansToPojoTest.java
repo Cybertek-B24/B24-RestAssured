634b22d2404201e7c691315695f5bce49a62c56d
Murodil <murodil@cybertekschool.com>
2022-02-03 21:07:36 -0500

    day 07: pojo

--- src/test/java/com/cybertek/tests/day07_deserialization/AllSpartansToPojoTest.java
+++ src/test/java/com/cybertek/tests/day07_deserialization/AllSpartansToPojoTest.java
@@ -3,6 +3,7 @@ package com.cybertek.tests.day07_deserialization;
 import com.cybertek.tests.SpartanTestBase;
 import com.cybertek.tests.pojo.AllSpartans;
 import com.cybertek.tests.pojo.Spartan;
+import com.cybertek.tests.pojo.SpartanSearch;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,7 @@ import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AllSpartansToPojoTest extends SpartanTestBase {
+
     @Test
     public void getAllSpartansTest() {
         Response response = given().accept(ContentType.JSON)
@@ -30,5 +32,27 @@ public class AllSpartansToPojoTest extends SpartanTestBase {
                 System.out.println(eachSpartan.toString());
             }
         }
+    }
+
+    @Test
+    public void searchSpartansToPojoTest() {
+        Response response = given().accept(ContentType.JSON)
+                .and().queryParam("nameContains", "ai")
+                .and().queryParam("gender", "Female")
+                .when().get("/api/spartans/search");
+
+        System.out.println("status code = " + response.statusCode());
+
+        //De-serialization. json body >> SpartanSearch java object
+        SpartanSearch spartanSearch = response.body().as(SpartanSearch.class);
+
+        System.out.println("total element = " + spartanSearch.getTotalElement());
+        System.out.println("spartans info content = " + spartanSearch.getContent());
+        System.out.println("spartan count = " + spartanSearch.getContent().size());
+
+        //store Jaimie info into separate spartan variable:
+        //Ferhat Demir — Today at 9:05 PM
+        Spartan first = spartanSearch.getContent().get(0);
+
     }
 }
---
+++ src/test/java/com/cybertek/tests/pojo/SpartanSearch.java
@@ -0,0 +1,14 @@
+package com.cybertek.tests.pojo;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class SpartanSearch {
+
+    private List<Spartan> content;
+
+    private int totalElement;
+
+}
