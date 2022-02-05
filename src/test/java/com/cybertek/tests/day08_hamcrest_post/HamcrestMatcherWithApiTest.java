b2ed4e364bf51eceb37561b4aeebe5f028038a5f
Murodil <murodil@cybertekschool.com>
2022-02-05 12:25:17 -0500

    day 08: hamcrest matchers

--- src/test/java/com/cybertek/tests/day08_hamcrest_post/HamcrestMatcherWithApiTest.java
+++ src/test/java/com/cybertek/tests/day08_hamcrest_post/HamcrestMatcherWithApiTest.java
@@ -1,4 +1,20 @@
 package com.cybertek.tests.day08_hamcrest_post;
 
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class HamcrestMatcherWithApiTest {
+    @Test
+    public void helloWorldApiTest() {
+        given().accept(ContentType.JSON)
+                .when().get("https://sandbox.api.service.nhs.uk/hello-world/hello/world")
+                .then().assertThat().statusCode(200)
+                .and().contentType("application/json")
+                .and().body("message" , equalTo("Hello World!"));
+    }
 }
