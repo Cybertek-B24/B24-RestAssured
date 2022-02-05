faf4fd493a94da3b16c0acd04a8c3d2380ea3235
Murodil <murodil@cybertekschool.com>
2022-02-05 12:45:31 -0500

    day 08: hamcrest matchers

--- src/test/java/com/cybertek/tests/day08_hamcrest_post/HamcrestMatcherWithApiTest.java
+++ src/test/java/com/cybertek/tests/day08_hamcrest_post/HamcrestMatcherWithApiTest.java
@@ -17,4 +17,18 @@ public class HamcrestMatcherWithApiTest {
                 .and().contentType("application/json")
                 .and().body("message" , equalTo("Hello World!"));
     }
+
+    //https://reqres.in/api/users
+    @Test
+    public void reqResAllUsersTest() {
+        given().accept(ContentType.JSON)
+                .when().get("https://reqres.in/api/users")
+                .then().assertThat().statusCode(200)
+                .and().contentType("application/json")
+                .and().body("total" , equalTo(12),
+                            "total_pages",is(2))
+                .and().body("data[0].first_name" , is(equalTo("George")),
+                            "data[0].email", is(equalTo("george.bluth@reqres.in")));
+    }
+
 }
