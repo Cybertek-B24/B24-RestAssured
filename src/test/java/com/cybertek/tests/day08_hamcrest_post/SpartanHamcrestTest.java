186e383c3760f315b3a94c9db3045354714a36db
Murodil <murodil@cybertekschool.com>
2022-02-05 15:17:50 -0500

    day 08: hamcrest matchers

--- src/test/java/com/cybertek/tests/day08_hamcrest_post/SpartanHamcrestTest.java
+++ src/test/java/com/cybertek/tests/day08_hamcrest_post/SpartanHamcrestTest.java
@@ -4,6 +4,8 @@ import com.cybertek.tests.SpartanTestBase;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
@@ -21,4 +23,33 @@ public class SpartanHamcrestTest extends SpartanTestBase {
                         "gender", is("Male"), "phone", is(1321321321))
                 .log().all();
     }
+    
+    @Test
+    public void getMapTest() {
+        /**
+         * in below example:
+         * send get request
+         * verify status code and header
+         * then convert json body to Map object and return
+         * DE-serialization
+         */
+      Map<String, Object> spartanMap =  given().accept(ContentType.JSON)
+                .when().get("/api/spartans/24")
+                .then().assertThat().statusCode(200)
+                .and().contentType("application/json").log().all()
+                .and().extract().body().as(Map.class); //convert json body to Map object and return
+
+        System.out.println("spartanMap = " + spartanMap);
+        //read id value from map then assert it is 24
+        assertThat(spartanMap.get("id"), equalTo(24));
+
+        //check the keys of json response
+        assertThat(spartanMap.keySet(), containsInAnyOrder("id","name","gender","phone"));
+
+        //check all values of json
+        assertThat(spartanMap.values(), hasItems(24, "Nadir", "Male" , 1321321321));
+    }
+
+
+    
 }
