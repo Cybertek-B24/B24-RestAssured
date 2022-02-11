aa3e350a2a4695fcc2e7c6b1bab4edc8d1f2833e
Murodil <murodil@cybertekschool.com>
2022-02-10 22:06:13 -0500

    DAY12: authentication

--- src/test/java/com/cybertek/tests/day12_api_authentication/LibraryApiAccessTokenTest.java
+++ src/test/java/com/cybertek/tests/day12_api_authentication/LibraryApiAccessTokenTest.java
@@ -1,6 +1,7 @@
 package com.cybertek.tests.day12_api_authentication;
 
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -28,6 +29,21 @@ public class LibraryApiAccessTokenTest {
                 .then().assertThat().statusCode(200)
                 .and().extract().body().path("token");
         System.out.println("token = " + token);
+    }
+
+    String accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7ImlkIjoiMTYwIiwiZnVsbF9uYW1lIjoiVGVzdCBTdHVkZW50IDMwIiwiZW1haWwiOiJzdHVkZW50MzBAbGlicmFyeSIsInVzZXJfZ3JvdXBfaWQiOiIzIn0sImlhdCI6MTY0NDU0NzQyMywiZXhwIjoxNjQ3MTM5NDIzfQ.8smrOO7spUfjq-DI_ABx0AAejUmulSUbb4zZccDiJ8A";
+
+    //https://library2.cybertekschool.com/rest/v1/get_book_list_for_borrowing
+
+    @Test
+    public void getBooksForBorrowing() {
+        Response response = given().accept(ContentType.JSON)
+                .and().header("x-library-token" , accessToken)
+                .when().get("https://library2.cybertekschool.com/rest/v1/get_book_list_for_borrowing");
+
+        System.out.println("status code = " + response.statusCode());
+        response.prettyPrint();
+
     }
 
 }
