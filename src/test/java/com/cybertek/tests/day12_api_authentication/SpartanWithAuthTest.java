ff33254517b6e2b5b6fb20c36c9a8dd01ede6068
Murodil <murodil@cybertekschool.com>
2022-02-10 19:55:13 -0500

    DAY12: authentication

---
+++ src/test/java/com/cybertek/tests/day12_api_authentication/SpartanWithAuthTest.java
@@ -0,0 +1,33 @@
+package com.cybertek.tests.day12_api_authentication;
+
+import com.cybertek.tests.SpartanTestBase;
+import com.cybertek.utilities.ConfigurationReader;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SpartanWithAuthTest {
+
+    @BeforeAll
+    public static void init() {
+        baseURI = ConfigurationReader.getProperty("spartan.auth.url");
+    }
+
+    /**
+     * Given accept type is json
+     * and basic auth credentials are "admin" , "admin"
+     * When I send get request to "/api/spartans"
+     * Then status code is 200
+     * and content type is json
+     * And json response should have Spartan list
+     */
+    @Test
+    public void adminGetAllSpartans() {
+
+    }
+}
