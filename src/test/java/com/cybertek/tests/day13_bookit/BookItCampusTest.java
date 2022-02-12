e1e3f9e72197832962236fdffc594d0b7f24e3e1
Murodil <murodil@cybertekschool.com>
2022-02-12 15:57:05 -0500

    DAY13: bookit

--- src/test/java/com/cybertek/tests/day13_bookit/BookItCampusTest.java
+++ src/test/java/com/cybertek/tests/day13_bookit/BookItCampusTest.java
@@ -3,6 +3,7 @@ package com.cybertek.tests.day13_bookit;
 import com.cybertek.tests.BookItTestBase;
 import com.cybertek.utilities.ConfigurationReader;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -11,17 +12,20 @@ import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BookItCampusTest extends BookItTestBase {
+
     @Test
-    public void getAllCampuses() {
+    public void getAllCampusesTest() {
         String accessToken = getAccessToken(ConfigurationReader.getProperty("teacher_email") ,
-                ConfigurationReader.getProperty("teacher_password") );
+                                            ConfigurationReader.getProperty("teacher_password") );
         System.out.println("accessToken = " + accessToken);
 
-        given().accept(ContentType.JSON)
+        JsonPath json = given().accept(ContentType.JSON)
                 .and().header("Authorization" , accessToken)
                 .when().get("/api/campuses")
-                .then().assertThat().statusCode(200);
+                .then().assertThat().statusCode(200)
+                .and().extract().jsonPath();
 
-
+        System.out.println(json.getList("clusters.rooms.name"));
+        System.out.println(json.getList("location"));
     }
 }
